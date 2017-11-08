39696d97a7aef980b03dd6c3915d65aa2bfbecd5
Victoria Bissengué <victoria.bissengue@canal-plus.com>
2017-11-08 12:55:50 +0100

    feat(homeWording): update wording

--- src/views/containers/Home/Home.data.jsx
+++ src/views/containers/Home/Home.data.jsx
@@ -119,8 +119,8 @@ const data = {
       ]
     },
     {
-      name: "clients",
-      title: { fr: "Clients", en: "Clients", jp: "クライアント" },
+      name: "clientsAgencies",
+      title: { fr: "Clients & Agences", en: "Clients & Agencies", jp: "クライアントとエージェンシー" },
       text: {
         fr: "J'ai eu le plaisir de travailler avec HBO, Netflix, Google, Canal+, Dinahmoe & plus",
         en: "I’ve had the pleasure of working with HBO, Netflix, Google, Canal+, Dinahmoe & more",

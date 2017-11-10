9746a7cd064d2b01f05c9a876b5708ec28967ebb
Victoria Bissengué <victoria.bissengue@canal-plus.com>
2017-11-10 11:53:08 +0100

    feat(datGui): auto-close on start

--- src/internal/synth.js
+++ src/internal/synth.js
@@ -113,6 +113,7 @@ function getSynth () {
     f2.open();
     f2f1.open();
     f2f2.open();
+    gui.close();
 
     [c1, c2, c3, c4, c5, c6, c7, c8, c9, c10, c11, c12, c13, c14, c15, c16, c17, c18, c19, c20, c21, c22, c23, c24, c25].forEach((controller) => {
       controller.onChange(() => { synth.set(settings); });

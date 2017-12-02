ab4f52814a12e47bd90ed15583c6e30bfd6ba469
Victoria Bissengué <victoria.bissengue@canal-plus.com>
2017-12-02 17:03:30 +0100

    feat(note): add burst animation

--- src/views/components/Note/Note.jsx
+++ src/views/components/Note/Note.jsx
@@ -1,10 +1,38 @@
 import { h, Component } from 'preact';
+import mojs from 'mo-js';
 
 import styles from './Note.css';
 
 class Note extends Component {
   componentDidMount () {
+    this.burst = new mojs.Burst({
+      parent: this.DOM,
+      radius: { 10: 12 },
+      count: 6,
+      children: {
+        shape: 'line',
+        fill : 'white',
+        radius: { 4: 0 },
+        scale: 1,
+        stroke: 'currentColor',
+        strokeWidth: 1,
+        duration: 1500,
+        easing: mojs.easing.bezier(0.1, 1, 0.3, 1),
+        opacity: 0.5
+      },
+      onComplete: () => {
+        setTimeout(() => {
+          this.burst.replay();
+        }, 500);
+      }
+    });
+
+    this.burst.play();
     this.props.onMount(this.DOM);
+  }
+
+  componentWillUnmount () {
+    this.burst.stop();
   }
 
   setDOM = (ref) => {
